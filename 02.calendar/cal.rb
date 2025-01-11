20eb04f00105e7bb60c61ef036865bb7aa8525c2
ex_SOUL <ex.soul.1984.cosmos@gmail.com>
2025-01-12 00:40:26 +0900

    Calendarクラスを作成し各処理をメソッドに分割した。
    * カレンダー1行目をcenterメソッドを使って中央揃えするようにした
    * 当日が含まれている場合に背景と文字色が反転する処理を追加

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -14,36 +14,65 @@ require 'optparse'
 
 WDAY_JA = %w[日 月 火 水 木 金 土].freeze
 
-opt = OptionParser.new
+class Calendar
+  def print_cal
 
-opt.on('-m MONTH') { |month| @month = month.to_i }
-opt.on('-y YEAR') { |year| @year = year.to_i }
+    puts cal_header
 
-opt.parse!(ARGV)
+    puts cal_weekdays
 
-@month ||= Date.today.month
-@year ||= Date.today.year
+    # 1週間で分割して出力
+    cal_days.each_slice(7) do |week|
+      puts week.join(' ')
+    end
+  end
 
-first_wday = Date.new(@year, @month, 1).wday
-last_day = Date.new(@year, @month, -1).day
+  private
 
-# calの1行目の月と年を出文字列
-cal_line1 = "      #{@month}月 #{@year}"
-puts cal_line1
+  def initialize
+    opt = OptionParser.new
 
-# calの2行目の曜日を出力
-cal_line2 = WDAY_JA.join(' ')
-puts cal_line2
+    opt.on('-m MONTH') { |month| @month = month.to_i }
+    opt.on('-y YEAR') { |year| @year = year.to_i }
 
-# 1日までの空白を追加
-cal_days = Array.new(first_wday, '  ')
+    opt.parse!(ARGV)
 
-# 1日から月末までの日付を追加
-(1..last_day).each do |day|
-  cal_days << format('%2d', day)
+    @today = Date.today
+    @month ||= @today.month
+    @year ||= @today.year
+  end
+
+  def cal_header
+    "#{@month}月 #{@year}".center(20)
+  end
+
+  def cal_weekdays
+    WDAY_JA.join(' ')
+  end
+
+  def cal_days
+    first_wday = Date.new(@year, @month, 1).wday
+    last_day = Date.new(@year, @month, -1).day
+
+    # 1日の曜日が一致するように日付配列に空白を追加
+    cal_days = Array.new(first_wday, '  ')
+
+    (1..last_day).each do |day|
+      cal_days << format('%2d', day)
+    end
+
+    if include_today?
+      today_day = @today.strftime('%2d')
+      today_index = cal_days.find_index(today_day)
+      cal_days[today_index] = "\e[7m#{today_day}\e[0m"
+    end
+
+    cal_days
+  end
+
+  def include_today?
+    @today.year == @year && @today.month == @month
+  end
 end
 
-# 1週間で分割して出力
-cal_days.each_slice(7) do |week|
-  puts week.join(' ')
-end
+Calendar.new.print_cal
